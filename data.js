ca5933a05b5b041a0279a9cc633d4fa8eb0dc48b
Canexxx <black-ori@hotmail.de>
2025-06-25 12:48:19 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+const geojsonData = {
+  "type": "FeatureCollection",
+  "features": [
+    {"type": "Feature", "properties": {"name": "Achadas da Cruz"}, "geometry": {"type": "Point", "coordinates": [-17.1875, 32.8384]}},
+    {"type": "Feature", "properties": {"name": "Pico do Arieiro"}, "geometry": {"type": "Point", "coordinates": [-16.9281, 32.7356]}},
+    {"type": "Feature", "properties": {"name": "Pico Ruivo"}, "geometry": {"type": "Point", "coordinates": [-16.9344, 32.7606]}},
+    {"type": "Feature", "properties": {"name": "Levada do Caldeirão Verde"}, "geometry": {"type": "Point", "coordinates": [-16.8762, 32.7671]}},
+    {"type": "Feature", "properties": {"name": "25 Fontes & Risco"}, "geometry": {"type": "Point", "coordinates": [-17.1153, 32.7447]}},
+    {"type": "Feature", "properties": {"name": "Ponta de São Lourenço"}, "geometry": {"type": "Point", "coordinates": [-16.7038, 32.7374]}},
+    {"type": "Feature", "properties": {"name": "Vereda do Fanal"}, "geometry": {"type": "Point", "coordinates": [-17.1239, 32.7905]}},
+    {"type": "Feature", "properties": {"name": "Levada do Furado"}, "geometry": {"type": "Point", "coordinates": [-16.8761, 32.7353]}},
+    {"type": "Feature", "properties": {"name": "Levada dos Cedros"}, "geometry": {"type": "Point", "coordinates": [-17.1392, 32.7941]}},
+    {"type": "Feature", "properties": {"name": "Vereda dos Balcões"}, "geometry": {"type": "Point", "coordinates": [-16.8751, 32.7374]}},
+    {"type": "Feature", "properties": {"name": "Porto da Cruz – Machico"}, "geometry": {"type": "Point", "coordinates": [-16.8223, 32.7679]}},
+  ]
+};
